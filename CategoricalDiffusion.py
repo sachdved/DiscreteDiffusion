b2b7ded22a0813e53fa7db031f93f3e9b88a7981
sachdved <sachdved@gmail.com>
2023-09-08 15:15:05 -0500

    refactored objective function calc

--- CategoricalDiffusion.py
+++ CategoricalDiffusion.py
@@ -7,6 +7,12 @@ from architectures import *
 
 
 class CategoricalDiffusion(torch.nn.Module):
+    """
+    Base class for Categorical diffusion.
+    input:
+        noise_matrix: tensor, K x K states
+        denoiser: torch.nn.Module, outputs denoised samples
+    """
     def __init__(
         self,
         denoiser,
@@ -21,39 +27,41 @@ class CategoricalDiffusion(torch.nn.Module):
         vecs = torch.real(vecs)
         self.PxT = vecs[:, torch.argmax(vals)].unsqueeze(0).unsqueeze(1)
 
+
     def decode(self, noised_samples, ts):
+        """
+        Forward pass of denoiser
+        input:
+            noised_samples: tensor (num_time_steps, batch_size, seq_length, num_classes)
+            ts: tensor (num_time_steps, batch_size)
+        """
         self.y_pred = self.denoiser(noised_samples, ts)
-        
-    def L_T(self, noised_sample):        
-        dkl_steady_state = torch.sum(
-            (noised_sample+1e-6) * torch.log((noised_sample+1e-6)/(self.PxT+1e-6))
+
+    def calc_forward_conditionals(self, noised_samples):
+        """
+        computes forward conditionals of the noised samples
+        input:
+            noised_samples: tensor (num_time_steps, batch_size, seq_length, num_classes)
+        """
+        forward_conditionals = torch.zeros(noised_samples.shape)
+
+        for t in range(forward_conditionals.shape[0]):
+            forward_conditionals[t] = torch.matmul(noised_samples[0], self.noise_matrix.matrix_power(t))
+            
+        self.forward_conditionals = forward_conditionals
+
+    def one_step_reverse_conditional(self, real, noised_sample):
+        """
+        computes the one step reverse conditional
+        input:
+            real: tensor, (batch_size, seq_length, num_states)
+            noised_samples: tensor, (num_time_steps, batch_size, seq_length, num_states)
+        output:
+            reverse_conditionals: tensor, (num_time_steps-2, batch_size, seq_length, num_states)
+        """
+        reverse_conditionals = torch.zeros(
+            noised_samples.shape[0]-2, noised_samples.shape[1], noised_samples.shape[2], noised_samples.shape[3]
         )
-    
-        return dkl_steady_state
-
-    def recon_loss(self, noised_samples):
-        real = noised_samples[0].tile(noised_samples.shape[0]-1, 1, 1, 1)
-        return self.cross_entropy(real, self.y_pred[1:])
-        
-    
-    def L_t0t1(self, real):
-        t_ones = ts[1].unsqueeze(0)
-        y_pred = self.y_pred[1]
-        return self.cross_entropy(real, self.y_pred)
-
-    def L_tminus1(self, real, noised_sample):
-        
-        reverse_marginals = self.one_step_reverse_marginal(real, noised_sample)
-        denoised = self.y_pred
-        px_tminus1_giv_xt = self.px_tminus1_giv_xt(noised_samples) 
-
-        return torch.sum(reverse_marginals * torch.log((reverse_marginals+1e-6)/(px_tminus1_giv_xt+1e-6)))
-
-    def cross_entropy(self, real, y_pred):
-        return -torch.sum(real * torch.log(y_pred+1e-6))
-
-    def one_step_reverse_marginal(self, real, noised_sample):
-        reverse_marginals = torch.zeros(noised_samples.shape[0]-2, noised_samples.shape[1], noised_samples.shape[2], noised_samples.shape[3])
         x0=real
         for t in range(2, noised_samples.shape[0]):
             
@@ -61,20 +69,77 @@ class CategoricalDiffusion(torch.nn.Module):
             numer = torch.matmul(xt, noise_matrix.t()) * torch.matmul(x0, self.noise_matrix.matrix_power(t-1))
             denom = torch.matmul(torch.matmul(x0, self.noise_matrix.matrix_power(t)), xt.permute(0,2,1))
             denom = torch.diagonal(denom, dim1=-2, dim2=-1).unsqueeze(-1)
-            reverse_marginals[t-2] = numer/denom
+            reverse_conditionals[t-2] = numer/denom
             
-        return reverse_marginals
+        return reverse_conditionals
 
-    def px_tminus1_giv_xt(self, noised_samples):
-        denoised = self.y_pred
-        px_onestepback = torch.zeros(denoised.shape[0]-2, denoised.shape[1], denoised.shape[2], denoised.shape[3])
+    def q_xtminus1_xt_giv_x0(self, noised_samples, reverse_conditionals):
         
-        for t in range(2, denoised.shape[0]):
-            
-            denoised_estimate = denoised[t]
-            real = noised_samples[0]
-            weighted_expectation = real*denoised_estimate
+        q_xtminus1_xt_giv_x0 = torch.zeros(
+            (noised_samples.shape[0]-2, noised_samples.shape[1], noised_samples.shape[2], noised_samples.shape[3])
+        )
 
-            px_onestepback[t-2] = torch.matmul(weighted_expectation, self.noise_matrix.matrix_power(t-1))
-        return px_onestepback
+        for t in range(q_xtminus1_xt_giv_x0.shape[0]):
+            q_xtminus1_xt_giv_x0[t] = reverse_conditionals[t] * torch.matmul(noised_samples[0], self.noise_matrix.matrix_power(t+2))
+
+        return q_xtminus1_xt_giv_x0
+
+    def L_T(self, noised_sample): 
+        """
+        computes D_KL of noised samples from the steady state distribution. Not relevant for gradient updates
+        input:
+            noised_samples: tensor (num_time_steps, batch_size, seq_length, num_classes)
+        output:
+            dkl_steady_state: tensor, (1, grad=None)
+        """
+        dkl_steady_state = torch.mean(
+            torch.sum(
+                (noised_sample+1e-6) * torch.log((noised_sample+1e-6)/(self.PxT+1e-6)),
+                axis=-1
+            )
+        )
     
+        return dkl_steady_state        
+    
+    def L_t0t1(self, real):
+        """
+        computes cross entropy of the one step decoder. requires self.decode and self.calc_forward_conditionals
+        input:
+            real: tensor, (batch_size, seq_length, states)
+        output:
+            one_step_cross_ent_loss: tensor, (1, grad=True)
+        """
+        y_pred = self.y_pred[1]
+        return -torch.mean(torch.sum(self.forward_conditionals[1] * torch.log(y_pred),axis=-1))
+
+        
+
+    def L_tminus1(self, real, noised_sample):
+        """
+        computes D_KL of each one step backwards decoding
+        input:
+            real: tensor, (batch_size, seq_length, states)
+            noised_sample: tensor, (num_time_steps, batch_size, seq_length, states)
+        output:
+            d_kl_per_time_step: tensor, (1, grad=True)
+        """
+        
+        reverse_conditionals = self.one_step_reverse_conditional(real, noised_sample)
+
+        
+        q_xtminus1_xt_giv_x0 = self.q_xtminus1_xt_giv_x0(noised_sample, reverse_conditionals)
+
+        denoised = self.y_pred[2:]
+
+        px_tminus1_giv_xt = q_xtminus1_xt_giv_x0 * denoised
+        
+        return torch.mean(
+            torch.sum(
+                self.forward_conditionals[2:] * (
+                    reverse_conditionals * torch.log(
+                        (reverse_conditionals + 1e-6)/(px_tminus1_giv_xt+1e-6)
+                    )
+                ),
+                axis=-1
+            )
+        )
